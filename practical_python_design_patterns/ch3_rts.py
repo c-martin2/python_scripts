ec0a36bffde9ee3d2b6e7380bae2cb695e9d8bd8
c-martin2 <cmartin2@cmartin2.com>
2018-04-14 18:51:24 -0400

    Test new type

--- practical_python_design_patterns/ch3_rts.py
+++ practical_python_design_patterns/ch3_rts.py
@@ -5,13 +5,18 @@ class Unit(UnitBase):
     def __init__(self, p_unit_type, p_level):
         self.unit_type = p_unit_type.capitalize()
 
-        with open('ch3_parms/' + self.unit_type.lower() + '_' + str(p_level)) as v_parm_file:
-            v_lines = v_parm_file.read().split('\n')
-            self.life = v_lines[0]
-            self.speed = v_lines[1]
-            self.attack_power = v_lines[2]
-            self.attack_range = v_lines[3]
-            self.weapon = v_lines[4]
+        try:
+            with open('ch3_parms/' + self.unit_type.lower() + '_' + str(p_level)) as v_parm_file:
+                v_lines = v_parm_file.read().split('\n')
+                self.life = v_lines[0]
+                self.speed = v_lines[1]
+                self.attack_power = v_lines[2]
+                self.attack_range = v_lines[3]
+                self.weapon = v_lines[4]
+        except Exception as v_error:
+            print(v_error.num)
+            print(v_error.msg)
+
 
     def __str__(self):
         return  'Type: ' + self.unit_type + '\n' \
@@ -39,5 +44,6 @@ if __name__ == '__main__':
     v_barracks = Barracks()
     v_k1 = v_barracks.build_unit('knight',1)
     v_a1 = v_barracks.build_unit('archer',2)
+    v_f1 = v_barracks.build_unit('footsoldier',1)
     print('[knight1] ' + str(v_k1))
     print('[archer1] ' + str(v_a1))
